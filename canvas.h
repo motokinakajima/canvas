d17838fa37160fc4d1b302c1b9d93212b1faa642
motokisama <motokinakajima0912@gmail.com>
2024-07-15 15:55:28 +0900

    reformet code

--- canvas.h
+++ canvas.h
@@ -1,5 +1,6 @@
 #ifndef GRAPHICS_CANVAS_H
 #define GRAPHICS_CANVAS_H
+
 #include <iostream>
 #include <cmath>
 #include <utility>
@@ -8,21 +9,22 @@
 class canvas {
 public:
     canvas() = default;
-    canvas(int x,int y){
-        data = std::vector<std::vector<int>>(y,std::vector<int>(x,0));
+
+    canvas(int x, int y) {
+        data = std::vector<std::vector<int>>(y, std::vector<int>(x, 0));
     }
 
-    void print_data(){
-        for(int i = 0;i < data.size();i++){
-            for(int j = 0;j < data.at(0).size();j++){
-                std::cout<<data[i][j]<<" ";
+    void print_data() {
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < data.at(0).size(); j++) {
+                std::cout << data[i][j] << " ";
             }
-            std::cout<<"\n";
+            std::cout << "\n";
         }
     }
 
-    void draw_pixel(int x, int y, int content){
-        if(y >= data.size() || y < 0 || x >= data.at(0).size() || x < 0){
+    void draw_pixel(int x, int y, int content) {
+        if (y >= data.size() || y < 0 || x >= data.at(0).size() || x < 0) {
             return;
         }
         data[y][x] = content;
@@ -38,10 +40,10 @@ public:
         }
     }
 
-    void draw_line(int x1,int y1, int x2, int y2, int thickness, int content){
-        if(std::abs(x2-x1)<std::abs(y2-y1)){
+    void draw_line(int x1, int y1, int x2, int y2, int thickness, int content) {
+        if (std::abs(x2 - x1) < std::abs(y2 - y1)) {
             //std::cout<<"y base"<<std::endl;
-            if(y1 > y2){
+            if (y1 > y2) {
                 int tmp_x = x1, tmp_y = y1;
                 tmp_x = x1;
                 tmp_y = y1;
@@ -51,13 +53,13 @@ public:
                 y2 = tmp_y;
             }
             //std::cout<<"x1: "<<x1<<" y1: "<<y1<<" x2: "<<x2<<" y2: "<<y2<<std::endl;
-            for(int i = y1; i <= y2; i++){
-                fill_circle(std::round((i-y1)*(x2+1-x1)/(y2+1-y1))+x1,i, thickness,content);
+            for (int i = y1; i <= y2; i++) {
+                fill_circle(std::round((i - y1) * (x2 + 1 - x1) / (y2 + 1 - y1)) + x1, i, thickness, content);
                 //std::cout<<"x: "<<std::round((i-y1)*(x2+1-x1)/(y2+1-y1))+x1<<" y: "<<i<<std::endl;
             }
-        }else{
+        } else {
             //std::cout<<"x base"<<std::endl;
-            if(x1 > x2){
+            if (x1 > x2) {
                 int tmp_x = x1, tmp_y = y1;
                 tmp_x = x1;
                 tmp_y = y1;
@@ -67,15 +69,17 @@ public:
                 y2 = tmp_y;
             }
             //std::cout<<"x1: "<<x1<<" y1: "<<y1<<" x2: "<<x2<<" y2: "<<y2<<std::endl;
-            for(int i = x1; i <= x2; i++){
-                fill_circle(i,std::round((i-x1)*(y2+1-y1)/(x2+1-x1))+y1,thickness,content);
+            for (int i = x1; i <= x2; i++) {
+                fill_circle(i, std::round((i - x1) * (y2 + 1 - y1) / (x2 + 1 - x1)) + y1, thickness, content);
                 //std::cout<<"x: "<<i<<" y: "<<std::round((i-x1)*(y2+1-y1)/(x2+1-x1))+y1<<std::endl;
             }
         }
     }
 
-    unsigned long long int get_width() const{ return data.at(0).size(); }
-    unsigned long long int get_height() const{ return data.size(); }
+    unsigned long long int get_width() const { return data.at(0).size(); }
+
+    unsigned long long int get_height() const { return data.size(); }
+
 private:
     std::vector<std::vector<int>> data;
 };
@@ -83,36 +87,37 @@ private:
 class model {
 public:
     model() = default;
-    model(int vertex_num,int side_num,int model_dimension){
+
+    model(int vertex_num, int side_num, int model_dimension) {
         dimension = model_dimension;
-        vertex = std::vector<std::vector<double>> (vertex_num,std::vector<double>(dimension));
-        side = std::vector<std::vector<double>> (side_num,std::vector<double>(2));
+        vertex = std::vector<std::vector<double>>(vertex_num, std::vector<double>(dimension));
+        side = std::vector<std::vector<double>>(side_num, std::vector<double>(2));
     }
 
-    void set_vertex(std::vector<std::vector<double>> input){
+    void set_vertex(std::vector<std::vector<double>> input) {
         vertex = std::move(input);
     }
 
-    void set_side(std::vector<std::vector<double>> input){
+    void set_side(std::vector<std::vector<double>> input) {
         side = std::move(input);
     }
 
-    std::vector<std::vector<double>> get_vertex(){
+    std::vector<std::vector<double>> get_vertex() {
         return vertex;
     }
 
-    std::vector<std::vector<double>> get_side(){
+    std::vector<std::vector<double>> get_side() {
         return side;
     }
 
-    canvas draw(canvas Canvas){
+    canvas draw(canvas Canvas) {
         dimension = vertex.at(0).size();
-        if(dimension != 2){
-            std::cout<<"This model is not 2d"<<std::endl;
+        if (dimension != 2) {
+            std::cout << "This model is not 2d" << std::endl;
             return Canvas;
         }
-        for(auto & i : side){
-            Canvas.draw_line(vertex[i[0]][0],vertex[i[0]][1],vertex[i[1]][0],vertex[i[1]][1],1);
+        for (auto &i: side) {
+            Canvas.draw_line(vertex[i[0]][0], vertex[i[0]][1], vertex[i[1]][0], vertex[i[1]][1], 1);
             //std::cout<<"first: "<<side[i][0]<<"  second: "<<side[i][1]<<std::endl;
             //std::cout<<"x1: "<<vertex[side[i][0]][0]<<"  y1: "<<vertex[side[i][0]][1]<<"  x2: "<<vertex[side[i][1]][0]<<"  y2: "<<vertex[side[i][1]][1]<<std::endl;
             //Canvas.print_data();
@@ -121,32 +126,33 @@ public:
         return Canvas;
     }
 
-    void print_data(){
-        for(int i = 0;i < vertex.size();i++){
-            std::cout<<"vertex["<<i<<"]: {";
-            for(int j = 0;j < vertex.at(0).size();j++){
-                if(j == vertex.at(0).size() - 1){
-                    std::cout<<vertex[i][j]<<"}"<<std::endl;
+    void print_data() {
+        for (int i = 0; i < vertex.size(); i++) {
+            std::cout << "vertex[" << i << "]: {";
+            for (int j = 0; j < vertex.at(0).size(); j++) {
+                if (j == vertex.at(0).size() - 1) {
+                    std::cout << vertex[i][j] << "}" << std::endl;
                     break;
-                }else{
-                    std::cout<<vertex[i][j]<<",";
+                } else {
+                    std::cout << vertex[i][j] << ",";
                 }
             }
         }
-        std::cout<<std::endl;
-        for(int i = 0;i < side.size();i++){
-            std::cout<<"side["<<i<<"]: {";
-            for(int j = 0;j < side.at(0).size();j++){
-                if(j == side.at(0).size() - 1){
-                    std::cout<<side[i][j]<<"}"<<std::endl;
+        std::cout << std::endl;
+        for (int i = 0; i < side.size(); i++) {
+            std::cout << "side[" << i << "]: {";
+            for (int j = 0; j < side.at(0).size(); j++) {
+                if (j == side.at(0).size() - 1) {
+                    std::cout << side[i][j] << "}" << std::endl;
                     break;
-                }else{
-                    std::cout<<side[i][j]<<",";
+                } else {
+                    std::cout << side[i][j] << ",";
                 }
             }
         }
-        std::cout<<std::endl<<std::endl;
+        std::cout << std::endl << std::endl;
     }
+
 private:
     std::vector<std::vector<double>> vertex;
     std::vector<std::vector<double>> side;
@@ -156,6 +162,7 @@ private:
 class camera {
 public:
     camera() = default;
+
     camera(std::vector<double> camera_coordinate, std::vector<double> camera_rotation, double camera_FOV)
             : coordinate(std::move(camera_coordinate)), rotation(std::move(camera_rotation)), FOV(camera_FOV) {}
 
@@ -163,75 +170,84 @@ public:
 
     std::vector<double> get_rotation() const { return rotation; }
 
-    void set_coordinate(std::vector<double>&& camera_coordinate) & {
+    void set_coordinate(std::vector<double> &&camera_coordinate) &{
         coordinate = std::move(camera_coordinate);
     }
 
-    void set_rotation(std::vector<double> camera_rotation) & {
+    void set_rotation(std::vector<double> camera_rotation) &{
         rotation = std::move(camera_rotation);
     }
 
-    std::vector<double> get_vector(){
+    std::vector<double> get_vector() {
         double y_rad = rotation[0] * (M_PI / 180.0);
         double z_rad = rotation[1] * (M_PI / 180.0);
-        return std::vector<double>{cos(z_rad)*-1*cos(y_rad),sin(z_rad)*-1*cos(y_rad),sin(y_rad)};
+        return std::vector<double>{cos(z_rad) * -1 * cos(y_rad), sin(z_rad) * -1 * cos(y_rad), sin(y_rad)};
     }
 
-    double get_FOV_by_rad() const{ return FOV * (M_PI / 180.0); }
+    double get_FOV_by_rad() const { return FOV * (M_PI / 180.0); }
 
-    double get_FOV_by_deg() const{ return FOV; }
+    double get_FOV_by_deg() const { return FOV; }
+
 private:
-    std::vector<double> coordinate {1,0,0};
-    std::vector<double> rotation {0,0};
+    std::vector<double> coordinate{1, 0, 0};
+    std::vector<double> rotation{0, 0};
     double FOV = 100;
 };
 
 class world {
 public:
     world(double max_width, double max_height, double max_depth, int max_item_num, camera world_camera)
-        : width(max_width), height(max_height), depth(max_depth), items(max_item_num, model()), Camera(std::move(world_camera)) {}
+            : width(max_width), height(max_height), depth(max_depth), items(max_item_num, model()),
+              Camera(std::move(world_camera)) {}
 
-    void add_item(model Model){
-        for(auto & item : items){
-            if(item.get_side().empty()){
+    void add_item(model Model) {
+        for (auto &item: items) {
+            if (item.get_side().empty()) {
                 item = std::move(Model);
                 break;
             }
         }
     }
-    void print_items(){
-        for(auto & item : items){
+
+    void print_items() {
+        for (auto &item: items) {
             item.print_data();
         }
     }
 
-    canvas render(int render_model_index, canvas render_canvas){
+    canvas render(int render_model_index, canvas render_canvas) {
         std::vector<std::vector<double>> model_vertex = items[render_model_index].get_vertex();
-        std::vector<std::vector<double>> rendered_vertex(model_vertex.size(),std::vector<double>(2,0));
-        for(int i = 0;i < model_vertex.size();i++){
-            rendered_vertex[i] = projection(model_vertex[i],render_canvas.get_width(),render_canvas.get_height());
+        std::vector<std::vector<double>> rendered_vertex(model_vertex.size(), std::vector<double>(2, 0));
+        for (int i = 0; i < model_vertex.size(); i++) {
+            rendered_vertex[i] = projection(model_vertex[i], render_canvas.get_width(), render_canvas.get_height());
         }
         model rendered_model;
         rendered_model.set_vertex(rendered_vertex);
         rendered_model.set_side(items[render_model_index].get_side());
-        std::cout<<"rendered model::"<<std::endl;
+        std::cout << "rendered model::" << std::endl;
         rendered_model.print_data();
         render_canvas = rendered_model.draw(std::move(render_canvas));
         return render_canvas;
     }
 
     double get_width() const { return width; }
+
     double get_height() const { return height; }
+
     double get_depth() const { return depth; }
+
     unsigned long long int get_max_item_num() const { return items.size(); }
-    std::vector<model> get_items(){ return items; }
-    camera get_camera(){return Camera;}
+
+    std::vector<model> get_items() { return items; }
+
+    camera get_camera() { return Camera; }
+
 private:
     double width, height, depth;
     std::vector<model> items;
     camera Camera;
 
-    std::vector<double> projection(std::vector<double> coordinate, double screen_width, double screen_height){
+    std::vector<double> projection(std::vector<double> coordinate, double screen_width, double screen_height) {
         std::vector<double> camera_coordinate = Camera.get_coordinate();
         std::vector<double> camera_vector = Camera.get_vector();
         double a = camera_vector[0];
@@ -291,8 +307,8 @@ private:
         }
 
         // Adjust lengths based on FOV and screen width
-        double lambda_length = (lambda * horizontal_vector_length) * projectionFactor * screen_width + screen_width/2;
-        double mu_length = (mu * vertical_vector_length) * projectionFactor * screen_width + screen_height/2;
+        double lambda_length = (lambda * horizontal_vector_length) * projectionFactor * screen_width + screen_width / 2;
+        double mu_length = (mu * vertical_vector_length) * projectionFactor * screen_width + screen_height / 2;
 
         return std::vector<double>{lambda_length, mu_length};
         /*
--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 
 int main() {
-    canvas Canvas = canvas(59,59);
+    canvas Canvas = canvas(59, 59);
     //Canvas.draw_line(1,1,10,10,1);
     //Canvas.print_data();
     ///*
@@ -11,19 +11,37 @@ int main() {
     //Rectangle.set_vertex({{1,1},{1,8},{8,1},{30,30}});
     //Rectangle.set_side({{0,1},{1,3},{0,2},{2,3}});
 
-    model cube = model(8,12,3);
-    cube.set_vertex({{3,1,-1},{3,1,1},{5,1,-1},{5,1,1},{3,-1,-1},{3,-1,1},{5,-1,-1},{5,-1,1}});
-    cube.set_side({{0,4},{2,6},{0,2},{4,6},{1,5},{3,7},{1,3},{5,7},{0,1},{2,3},{4,5},{6,7}});
+    model cube = model(8, 12, 3);
+    cube.set_vertex({{3, 1,  -1},
+                     {3, 1,  1},
+                     {5, 1,  -1},
+                     {5, 1,  1},
+                     {3, -1, -1},
+                     {3, -1, 1},
+                     {5, -1, -1},
+                     {5, -1, 1}});
+    cube.set_side({{0, 4},
+                   {2, 6},
+                   {0, 2},
+                   {4, 6},
+                   {1, 5},
+                   {3, 7},
+                   {1, 3},
+                   {5, 7},
+                   {0, 1},
+                   {2, 3},
+                   {4, 5},
+                   {6, 7}});
     //cube.print_data();
 
     //Canvas = Rectangle.draw(Canvas);
     //Canvas.print_data();
-    std::cout<<std::endl;
-    camera myCamera = camera({-2,0,0},{-10,20},100);
-    world World = world(100,100,100,3,myCamera);
+    std::cout << std::endl;
+    camera myCamera = camera({-2, 0, 0}, {-10, 20}, 100);
+    world World = world(100, 100, 100, 3, myCamera);
     World.add_item(cube);
     World.print_items();
-    Canvas = World.render(0,Canvas);
+    Canvas = World.render(0, Canvas);
     Canvas.print_data();
     //int flag;
     //flag = std::cin.get();
